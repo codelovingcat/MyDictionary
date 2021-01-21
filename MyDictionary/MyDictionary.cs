0f11b3585b3debb982bd4d64fb031d7b3a2bfc99
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2021-01-21 15:17:46 +0200

    MyDictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] _items;
+        public MyDictionary()
+        {
+            _items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = _items;
+            _items = new T[_items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _items[i] = tempArray[i];
+            }
+
+            _items[_items.Length - 1] = item;
+        }
+        public T[] List
+        {
+            get { return _items; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> names = new MyDictionary<string>();
+            names.Add("Ürem");
+            names.Add("Sancaktutan");
+
+
+            foreach (var name in names.List)
+            {
+                Console.WriteLine("Added! : " + name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
